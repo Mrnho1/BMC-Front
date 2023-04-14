ad42934959e7f098ac54c41e60430e7aedc3419c
Gabriel Farias <gabrielv.rfaria@gmail.com>
2023-04-14 13:16:04 -0300

    Inicio da implementação da tela de login

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Login from './paginas/login/Login';
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-    
-    </>
+    <Router>
+    <Routes>
+
+      <Route path="/" element={<Login  />} />
+
+      <Route path="/login" element={<Login />} />
+
+    </Routes>
+  </Router>
+
   )
 }
 
