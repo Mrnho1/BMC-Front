ef8604f1aa5f7a410686cf6996402be3b3903768
Laise France <laise94@hotmail.com>
2023-04-22 10:17:06 -0300

    montando estrutura page-produtos

--- src/paginas/produtos/Produtos.tsx
+++ src/paginas/produtos/Produtos.tsx
@@ -1,11 +1,39 @@
+import { Grid } from '@material-ui/core';
 import * as React from 'react';
 
 function Produtos() {
     return (
-        <div>
-            <h1>Produtos</h1>
-        </div>
-    )
+    <>
+      <Grid container spacing={1}>
+        <Grid item xs={12} sm={12}>
+          colocar aqui
+        </Grid>
+        <Grid item xs={12} sm={12}>
+          <h1>
+            oque colocar aqui?
+          </h1>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          e aqui
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          e aqui
+        </Grid>
+        <Grid item xs={12} sm={3}>
+          AQUI
+        </Grid>
+        <Grid item xs={12} sm={3}>
+         AQUI  
+        </Grid>
+        <Grid item xs={12} sm={3}>
+          AQUI
+        </Grid>
+        <Grid item xs={12} sm={3}>
+          AQUI
+        </Grid>
+      </Grid>
+    </>
+  );
 }
 
 export default Produtos;
