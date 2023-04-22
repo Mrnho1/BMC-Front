30fef9bda4a00c7d5251dd26c3c1e630ed03563d
Laise France <laise94@hotmail.com>
2023-04-22 15:22:38 -0300

    add redirecionamento do login e cadastro

--- src/components/staticos/navbartwo/Navbartwo.tsx
+++ src/components/staticos/navbartwo/Navbartwo.tsx
@@ -15,8 +15,10 @@ export default function  Navbartwo() {
           <Typography variant="h6" component="div" sx={{ padding: 0, margin: 0}}>
             
           </Typography>
-          <Button color="inherit">Login</Button>
-          <Button color="inherit">Contato</Button>
+          <Button onClick={() => window.location.href = '/login'} color="inherit">Login</Button>
+          
+          <Button onClick={() => window.location.href = '/cadastro'} color="inherit">Cadastro</Button>
+          
         </Toolbar>
       </AppBar>
     </Box>
