a28893dea146235d6401ab808004fac8054efee5
Laise France <laise94@hotmail.com>
2023-04-22 10:56:03 -0300

    add carousel de imagens

--- src/paginas/produtos/Produtos.tsx
+++ src/paginas/produtos/Produtos.tsx
@@ -1,17 +1,17 @@
 import { Grid } from '@material-ui/core';
 import * as React from 'react';
+import Navbartwo from '../../components/navbartwo/Navbartwo';
+import Carousel from '../../components/staticos/carousel/Carousel';
 
 function Produtos() {
     return (
     <>
       <Grid container spacing={1}>
         <Grid item xs={12} sm={12}>
-          colocar aqui
+         <Navbartwo />
         </Grid>
         <Grid item xs={12} sm={12}>
-          <h1>
-            oque colocar aqui?
-          </h1>
+            <Carousel />
         </Grid>
         <Grid item xs={12} sm={6}>
           e aqui
