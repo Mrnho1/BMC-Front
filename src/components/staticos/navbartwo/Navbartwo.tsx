7b73cd42a020d5f43ab21b8d574db36ffda92728
Gustavo Santiago <gustavosantiago81@yahoo.com.br>
2023-04-26 19:19:45 -0300

    navbarTwo functional, but without style

--- src/components/staticos/navbartwo/Navbartwo.tsx
+++ src/components/staticos/navbartwo/Navbartwo.tsx
@@ -6,34 +6,108 @@ import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import Link from "@mui/material/Link";
 import { makeStyles } from "@mui/material/styles";
-
-const pagesLinks = [
-  { name: "Home", href: "/home" },
-  { name: "Sobre", href: "/sobre" },
-  { name: "Produtos", href: "/produtos" },
-  { name: "Equipe", href: "/equipe" },
-  { name: "Contato", href: "/contato" },
-];
+import { Container, IconButton } from "@mui/material";
+import MenuItem from "@mui/material/MenuItem";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import ListItem from "@material-ui/core/ListItem";
+import List from "@material-ui/core/List";
+import { AlignHorizontalCenter } from "@mui/icons-material";
 
 export default function Navbartwo() {
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
+    null
+  );
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
+    null
+  );
+
+  const pagesLinksTwo = [
+    { name: "Home", href: "/home" },
+    { name: "Sobre", href: "/sobre" },
+    { name: "Produtos", href: "/produtos" },
+    { name: "Equipe", href: "/equipe" },
+    { name: "Contato", href: "/contato" },
+  ];
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
   return (
     <Box sx={{ padding: 0, margin: 0 }}>
       <AppBar position="static" style={{ backgroundColor: "#78a493" }}>
+      <Container maxWidth="xl">
         <Toolbar
           sx={{
             justifyContent: "center",
           }}
         >
-          <Box>
-            {pagesLinks.map((item) => (
+          <Box sx={{ justifyContent: "center", flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            >
+
+
+              {pagesLinksTwo.map((pageTw) => (
+                <List>
+                  <MenuItem key={pageTw.href} onClick={handleCloseNavMenu}>
+                    <ListItem>
+                      <Link href={pageTw.href}>
+                        <Typography textAlign="center">
+                          {pageTw.name}
+                        </Typography>
+                      </Link>
+                    </ListItem>
+                  </MenuItem>
+                </List>
+              ))}
+            </Menu>
+          </Box>
+
+          <Box sx={{ justifyContent: "center", flexGrow: 1, display: { xs: 'none', md: 'flex' }}}>
+            {pagesLinksTwo.map((item) => (
               <Button>
-                <Link href={item.href} marginX={20}>
+                
+                  <Link href={item.href} marginX={20}>
                   {item.name}
                 </Link>
+                
+                
               </Button>
             ))}
           </Box>
         </Toolbar>
+        </Container>
       </AppBar>
     </Box>
   );
