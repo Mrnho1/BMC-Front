244669709b46001f4234d3062b3dcf3f157a35f3
Danilo Moraes <danilot.moraes@gmail.com>
2023-04-26 10:19:18 -0300

    Implementacao da service post, put, get e delete

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -13,3 +13,27 @@ export const login = async(url: string, dados: Object, setDados: Function) => {
     const resposta = await api.post(url, dados)
     setDados(resposta.data.token)
 }
+
+export const getAll = async(url: string, setDados: Function, headers: any) => {
+    const resposta = await api.get(url, headers)
+    setDados(resposta.data)
+}
+
+export const getById = async(url: string, setDados: Function, headers: any) => {
+    const resposta = await api.get(url, headers)
+    setDados(resposta.data)
+}
+
+export const post = async(url: string, dados:any ,setDados: Function, headers: any) => {
+    const resposta = await api.post(url, dados, headers)
+    setDados(resposta.data)
+}
+
+export const put = async(url: string, dados:any ,setDados: Function, headers: any) => {
+    const resposta = await api.put(url, dados, headers)
+    setDados(resposta.data)
+}
+
+export const deleteId = async(url: string, headers: any) => {
+    await api.delete(url, headers)
+}
