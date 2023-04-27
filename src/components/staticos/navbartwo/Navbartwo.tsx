903c91a5a81caaca73f5b68e36105c18d52b9896
Gustavo Santiago Pereira <123910445+GustavoSantiago81@users.noreply.github.com>
2023-04-26 23:16:51 -0300

    Update Navbartwo.tsx

--- src/components/staticos/navbartwo/Navbartwo.tsx
+++ src/components/staticos/navbartwo/Navbartwo.tsx
@@ -100,7 +100,6 @@ export default function Navbartwo() {
                 ))}
               </Menu>
             </Box>
-
             <Box
               sx={{
                 justifyContent: "center",
