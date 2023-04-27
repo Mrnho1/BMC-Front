03f9e18e96c8ab79a4595687e3e133afbc8f55a4
Danilo Moraes <danilot.moraes@gmail.com>
2023-04-27 15:33:03 -0300

    Carousel produto

--- src/components/staticos/carousel/Carousel.tsx
+++ src/components/staticos/carousel/Carousel.tsx
@@ -1,139 +1,44 @@
 import * as React from 'react';
-import { useTheme } from '@mui/material/styles';
 import Box from '@mui/material/Box';
-import MobileStepper from '@mui/material/MobileStepper';
-import Paper from '@mui/material/Paper';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
-import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
-import SwipeableViews from 'react-swipeable-views';
-import { autoPlay } from 'react-swipeable-views-utils';
-import styled from '@emotion/styled';
-import imagem1 from './imagens-carousel/imagem1.jpg'
 
-const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
+import 'react-alice-carousel/lib/alice-carousel.css';
+import AliceCarousel from 'react-alice-carousel';
 
-// https://ibb.co/NmbvHrC
-// https://ibb.co/Lh0NM4d
-// https://ibb.co/ZGx8Wcv
-// https://ibb.co/C8m7KS7
-// https://ibb.co/SRQ5qhN
-// https://ibb.co/523DSMS
-// https://ibb.co/gtmTtbc
 
-const images = [
-  {
-    label: '',
-    imgPath: 'https://images.unsplash.com/photo-1606226287089-5b5896241e11?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80' ,
-    size: 'large',
-    },
-  {
-    label: '',
-    imgPath:
-      'https://images.unsplash.com/photo-1603712426309-1a050b0378c8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
-    size: 'large',
-    },
-  {
-    label: '',
-    imgPath: 'https://images.unsplash.com/photo-1612882514847-57bcc4223456?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
-    size: 'large',
-  },
-  {
-    label: '',
-    imgPath:
-      'https://images.unsplash.com/photo-1613732899584-22b68758d40b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1179&q=80',
-    size: 'large',
-    },
-];
 
 function Carousel() {
-  const theme = useTheme();
-  const [activeStep, setActiveStep] = React.useState(0);
-  const maxSteps = images.length;
+  const items = [
+    <>
+      <img src="https://github.com/tjfaccipieri.png" alt="" />
+      <h2>Thiago Faccipieri</h2>
+    </>,
+    <img src="https://github.com/Wall90s.png" alt="" />,
+    <img src="https://github.com/kaluribr.png" alt="" />,
+    <img src="https://github.com/JaineSantos0.png" alt="" />,
+    <img src="https://github.com/ElizangelaXavierS.png" alt="" />,
+  ]
 
-  const handleNext = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
-  };
-
-  const handleBack = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep - 1);
-  };
-
-  const handleStepChange = (step: number) => {
-    setActiveStep(step);
-  };
+  const responsivo = {
+    0: {
+        items: 1,
+    },
+    1024: {
+        items: 2,
+        itemsFit: 'contain',
+    }
+  }
 
   return (
-    // carousel ocupa toda a largura da tela
-    <Box sx={{ width: '100%', flexGrow: 1 , padding: 0, margin: 0}}>
-      <Paper
-        square
-        elevation={0}
-        sx={{
-          display: 'flex',
-          alignItems: 'center',
-          pl: 2,
-          bgcolor: 'background.default',
-        }}
-      >
-        <Typography>{images[activeStep].label}</Typography>
-      </Paper>
-      <AutoPlaySwipeableViews
-        axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
-        index={activeStep}
-        onChangeIndex={handleStepChange}
-        enableMouseEvents
-      >
-        {images.map((step, index) => (
-          <div key={step.label}>
-            {Math.abs(activeStep - index) <= 2 ? (
-              <Box
-                component="img"
-                sx={{
-                  height: 580,
-                  display: 'block',
-                  width: '100%',
-                  overflow: 'hidden',
-                  
-                }}
-                src={step.imgPath}
-                alt={step.label}
-              />
-            ) : null}
-          </div>
-        ))}
-      </AutoPlaySwipeableViews>
-      <MobileStepper
-        steps={maxSteps}
-        position="static"
-        activeStep={activeStep}
-        nextButton={
-          <Button
-            size="small"
-            onClick={handleNext}
-            disabled={activeStep === maxSteps - 1}
-          >
-            Next
-            {theme.direction === 'rtl' ? (
-              <KeyboardArrowLeft />
-            ) : (
-              <KeyboardArrowRight />
-            )}
-          </Button>
-        }
-        backButton={
-          <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
-            {theme.direction === 'rtl' ? (
-              <KeyboardArrowRight />
-            ) : (
-              <KeyboardArrowLeft />
-            )}
-            Back
-          </Button>
-        }
-      />
-    </Box>
+    
+    <AliceCarousel
+      mouseTracking
+      items={items}
+      autoPlay
+      infinite
+      responsive={responsivo}
+      disableDotsControls={true}
+      disableButtonsControls={true}
+      autoPlayInterval={2500} />
   );
 }
 
