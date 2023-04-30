ff4051d5fb1c1fb95388a84b25dc40e5ff0a37c8
Gustavo Santiago <gustavosantiago81@yahoo.com.br>
2023-04-30 17:51:23 -0300

    ok, without asyng categoria

--- src/components/produtos/cadastroProduto/CadastroProduto.tsx
+++ src/components/produtos/cadastroProduto/CadastroProduto.tsx
@@ -19,31 +19,6 @@ import "./CadastroProduto.css";
 import { InputNumber } from "primereact/inputnumber";
 
 function CadastroProduto() {
-  const [value, setValue] = useState("");
-
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const inputValue = event.target.value;
-
-    // Remove todos os caracteres não numéricos
-    const numericValue = inputValue.replace(/\D/g, "");
-
-    // Formata o valor para a máscara de moeda
-    const formattedValue = formatCurrency(numericValue);
-
-    setValue(formattedValue);
-    updateProduto(event);
-  };
-
-  const formatCurrency = (value: string) => {
-    const numberFormat = new Intl.NumberFormat("pt-BR", {
-      style: "currency",
-      currency: "BRL",
-      minimumFractionDigits: 2,
-    });
-
-    return numberFormat.format(Number(value) / 100);
-  };
-
   const history = useNavigate();
   const [categorias, setCategorias] = useState<Categoria[]>([]);
   const token = useSelector<TokenState, TokenState["tokens"]>(
